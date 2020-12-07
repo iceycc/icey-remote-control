6441b13d0f4f80ed201af1f66b66f0eaf321b702
wangbingyang <wangbingyang@doushen.com>
2020-12-07 14:43:20 +0800

    update

--- client/app/main/windows/main.js
+++ client/app/main/windows/main.js
@@ -1,20 +1,21 @@
-const {BrowserWindow}=require('electron');
-const isDev=require('electron-is-dev');//判断是生产环境还是开发环境
-const path=require('path')
-let win,winAll=false;
-function create(){
-    win=new BrowserWindow({//创建一个窗口
-        width:600,
-        height:600,
-        webPreferences:{//可以使用node相关的
-            nodeIntegration:true
+const {BrowserWindow} = require('electron');
+const isDev = require('electron-is-dev');//判断是生产环境还是开发环境
+const path = require('path')
+let win, winAll = false;
+
+function create() {
+    win = new BrowserWindow({//创建一个窗口
+        width: 600,
+        height: 600,
+        webPreferences: {//可以使用node相关的
+            nodeIntegration: true
         }
     })
 
-    win.on('close',(e)=>{
-        if(winAll){//是否是真正退出
-            win=null
-        }else{
+    win.on('close', (e) => {
+        if (winAll) {//是否是真正退出
+            win = null
+        } else {
             e.preventDefault()//禁止窗口的关闭
             win.hide()//窗口隐藏
         }
@@ -23,19 +24,21 @@ function create(){
     // if(isDev){
     //     win.loadURL('http://localhost:3000')
     // }else{
-        win.loadFile(path.resolve(__dirname,"../../renderer/pages/main/index.html"))
+    win.loadFile(path.resolve(__dirname, "../../renderer/pages/main/index.html"))
     // }
 }
-function send(channel,...args){//接收信息
-    win.webContents.send(channel,...args)
+
+function send(channel, ...args) {//接收信息
+    win.webContents.send(channel, ...args)
 }
 
-function show(){
+function show() {
     win.show()
 }
-function close(){
-    winAll=true;//应用真正关闭
+
+function close() {
+    winAll = true;//应用真正关闭
     win.close()
 }
 
-module.exports={create,send,close,show};
+module.exports = {create, send, close, show};
